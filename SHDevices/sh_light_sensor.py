5c5f678481abb2ac4586fd0e66c250392d3bcc46
Christoph Gollok <christoph.gollok@gmail.com>
2022-05-01 21:13:44 +0200

    add simple lightsensor

---
+++ SHDevices/sh_light_sensor.py
@@ -0,0 +1,44 @@
+from SHDevices.sh_device import *
+
+class SH_Light_Sensor(SHDevice):
+
+    def __init__(self,name, connection=None, device=None, states={}, fields={}):
+        super().__init__(name, connection, states, fields)
+                
+        # add states
+        super().add_state('luminosity',0)
+
+        # add fields
+        #super().add_field('position',device.getField("pointLightColor"))
+
+        self.sensor = device.getDevice("light sensor")
+        self.sensor.enable(64)
+
+        self.reset()    
+
+    def updateLuminosity(self,value):
+        self.log("new Luminosity -> " + str(value))
+        self.states['luminosity'] = value
+        self.send(self.toJSON())
+
+    def setState(self, name, value):    
+        super().setState(name, value)
+
+        match name:
+            case "luminosity":
+                # read only value
+                pass
+            case _:
+                print("state not found or state is read only")
+                pass
+
+        self.send(self.toJSON())
+    
+    def register(self):
+        super().register()
+        self.connection.send(self.toJSON())
+
+
+    def reset(self):
+        # self.set_state('setPosition',1.3)
+        pass
---
+++ controllers/LightSensorController/LightSensorController.py
@@ -0,0 +1,69 @@
+"""Light Sensor controller."""
+
+from controller import Robot
+from controller import Supervisor
+
+from SHDevices.sh_device import *
+from SHDevices.sh_light_sensor import *
+
+import json
+from config.definitions import ROOT_DIR, _CFG
+
+def connected(ws):
+    global sh_device
+    sh_device.log("Connected")
+    sh_device.register()
+
+    
+def error(ws, error):
+    global sh_device
+    sh_device.log("Connection Error -> " + str(error))
+
+def closed(ws, close_status_code, close_msg):
+    global sh_device
+    sh_device.log("Disconected")
+    pass
+
+def message_cb(ws, message):
+    global sh_device
+    sh_device.log("new message -> " + message)
+    try:
+        msg = json.loads(message)
+        sh_device.setState(msg["property"], msg["value"])
+    except Exception as e:
+        sh_device.log(str("Message Error: " + str(e)))
+        return
+
+TIME_STEP = 64
+robot = Supervisor()
+timestep = int(robot.getBasicTimeStep())
+print(_CFG)
+
+# create connection object
+ws = WebSocketClient(uri=_CFG["websocket"]["url"],open_cb=connected,
+                                                 close_cb=closed,
+                                                 message_cb=message_cb,
+                                                 error_cb=error) 
+ws.start()
+
+# create instance of SmartHome Device
+sh_device = SH_Light_Sensor(robot.getName(), connection=ws,device=robot)
+
+last_luminosity = -1
+
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    
+    current_luminosity = sh_device.sensor.getValue()
+
+    if current_luminosity != last_luminosity:
+        last_luminosity = current_luminosity
+        sh_device.updateLuminosity(current_luminosity)
+         
+    
+    
+    pass
+
+# cleanup on Exit
