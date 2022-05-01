191f09da9ac79596be997aef82b2e5edc4f270e7
Christoph Gollok <christoph.gollok@gmail.com>
2022-05-01 15:46:10 +0200

    add template classes for controller and devices

---
+++ SHDevices/sh_controller_template.py
@@ -0,0 +1,65 @@
+"""SH Controller Template controller."""
+
+from controller import Robot
+from controller import Supervisor
+
+from SHDevices.sh_device import *
+# from SHDevices.sh_shutter import *
+
+import json
+from config.definitions import ROOT_DIR, _CFG
+
+
+def log(msg):
+    global robot
+    print(robot.getName() + ":" + msg)
+
+def connected(ws):
+    global sh_device
+    sh_device.log("Connected")
+    sh_device.register()
+
+    
+def error(ws, error):
+    global sh_device
+    sh_device.log("Connection Error -> " + str(error))
+
+def closed(ws, close_status_code, close_msg):
+    global sh_device
+    sh_device.log("Disconected")
+    pass
+
+def message_cb(ws, message):
+    global sh_device
+    sh_device.log("new message -> " + message)
+    try:
+        msg = json.loads(message)
+        sh_device.setState(msg["property"], msg["value"])
+    except Exception as e:
+        sh_device.log(str("Message Error: " + str(e)))
+        return
+
+TIME_STEP = 64
+robot = Supervisor()
+timestep = int(robot.getBasicTimeStep())
+print(_CFG)
+
+
+# create instance of SmartHome Device
+# sh_device = SH_Shutter(robot.getName(),device=robot)
+
+# create connection object
+# ws = WebSocketClient(uri=_CFG["websocket"]["url"],open_cb=connected ,close_cb=closed,message_cb=message_cb, error_cb=error)
+# ws.start()
+
+# assign connection to SmartHome Device
+# sh_device.connection = ws
+
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    
+    pass
+
+# cleanup on Exit
---
+++ SHDevices/sh_template.py
@@ -0,0 +1,46 @@
+from sh_device import SHDevice
+
+class SH_Shutter(SHDevice):
+
+    def __init__(self,name, connection=None, device=None, states={}, fields={}):
+        super().__init__(name, connection, states, fields)
+                
+        # add states
+        # super().add_state('setPosition',1.3)
+
+        # add fields
+        #super().add_field('position',device.getField("pointLightColor"))
+
+
+        self.reset()    
+
+    # 
+    def setState(self, name, value):    
+        super().setState(name, value)
+
+        match name:
+            case "setPosition":
+                # self.setPosition(value)
+                pass
+            case "up":
+                # self.setUp(value)
+            case "down":
+                # self.setDown(value)
+                pass
+            case "stop":
+                # self.setStop(value)
+                pass
+            case _:
+                print("state not found or state is read only")
+                pass
+
+        self.send(self.toJSON())
+    
+    def register(self):
+        super().register()
+        self.connection.send(self.toJSON())
+
+
+    def reset(self):
+        # self.set_state('setPosition',1.3)
+        pass
