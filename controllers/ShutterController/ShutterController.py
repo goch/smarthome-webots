9d879d843eeb71dba7daac25110d977beb2011f3
Christoph Gollok <christoph.gollok@gmail.com>
2022-05-01 15:47:37 +0200

    add send functionality to shutter Controller

--- controllers/ShutterController/ShutterController.py
+++ controllers/ShutterController/ShutterController.py
@@ -5,14 +5,41 @@
 from controller import Robot
 from controller import Supervisor
 
+import os
+from config.definitions import ROOT_DIR, _CFG
+from SHDevices.sh_device import *
+from SHDevices.sh_shutter import *
+import json
 
 
-import struct
-from sh_shutter import *
-from sh_device import *
-from sh_shutter import *
+def log(msg):
+    global robot
+    print(robot.getName() + ":" + msg)
 
+def connected(ws):
+    global shutter
+    log("Connected")
+    shutter.register()
+    #msg = device.toJSON()
+    
+def error(ws, error):
+    global shutter
+    shutter.log("Connection Error -> " + str(error))
 
+def closed(ws, close_status_code, close_msg):
+    global shutter
+    log("Disconected")
+    pass
+
+def message_cb(ws, message):
+    global shutter
+    log("new message -> " + message)
+    try:
+        msg = json.loads(message)
+        shutter.setState(msg["property"], msg["value"])
+    except Exception as e:
+        log(str("Message Error: " + str(e)))
+        return
 
 TIME_STEP = 64
 # create the Robot instance.
@@ -21,34 +48,39 @@ robot = Supervisor()
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
+
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
 #  motor = robot.getDevice('motorname')
 #  ds = robot.getDevice('dsname')
 #  ds.enable(timestep)
 
-emitter = robot.getDevice("emitter")
-receiver = robot.getDevice("receiver")
-receiver.enable(100)
 
-shutter = SH_Shutter(robot.getName(),robot,emitter,receiver)
-shutter.register()
-
-
-# motor = robot.getDevice("linear motor")
+# motor = robot.getDevice('linear motor')
+# position = robot.getDevice("position sensor")
 # motor.setPosition(1.3)
+#position.enable(TIME_STEP)
+
+shutter = SH_Shutter(robot.getName(),device=robot)
+
+ws = WebSocketClient(uri=_CFG["websocket"]["url"],open_cb=connected ,close_cb=closed,message_cb=message_cb, error_cb=error)
+ws.start()
+shutter.connection = ws
+
+
+lastPosition = 0 
+delta = (shutter.motor.getMaxPosition() - shutter.motor.getMinPosition()) / 25
+
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    while receiver.getQueueLength() > 0:
-        
-        print( robot.getName() + ": -- DATA PACKET RECEIVED! --")
-        msg = receiver.getData()
-        print(msg)
-        
-        
-        #if robot.getName() == "SH_SHUTTER":      
-        #receiver.nextPacket()
+    
+    currentPosition = shutter.motor_position.getValue()
+    currentDelta = abs(lastPosition - currentPosition)
+    
+    if currentDelta >= delta:
+        lastPosition = currentPosition
+        shutter.updateCurrentPosition(currentPosition)
 
     pass
 
