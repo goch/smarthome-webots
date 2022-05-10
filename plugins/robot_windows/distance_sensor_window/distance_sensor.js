ce550f174f201d2eda7dbfab17de9867c19238a3
Christoph Gollok <christoph.gollok@alumni.fh-aachen.de>
2022-05-10 19:18:33 +0200

    add triggered value to webui

--- plugins/robot_windows/distance_sensor_window/distance_sensor.js
+++ plugins/robot_windows/distance_sensor_window/distance_sensor.js
@@ -4,8 +4,19 @@
 const label_device_name = document.querySelector("#device_name");
 const label_current_distance = document.querySelector("#currentDistance");
 const input_trigger_distance = document.querySelector("#input_triggerDistance");
+const label_triggered = document.querySelector("#label_triggered");
 
 let trigger_distance = -1.0
+
+
+function setTriggered(triggered){
+    label_triggered.innerHTML = triggered.toString();
+    if (triggered){
+        label_triggered.style.background = "green";
+    }else{
+        label_triggered.style.background = "red";
+    } 
+}
 
 function setDistance(distance){
     label_current_distance.innerHTML = distance.toFixed(3) + " m";
@@ -31,6 +42,7 @@ function on_message(message) {
             label_device_name.innerHTML = msg['name'];
             setDistance(msg['data']["distance"]);
             setInputText(msg['data']["trigger_distance"])
+            setTriggered(msg['data']['triggered'])
         } catch (e) {}
     }
 }
