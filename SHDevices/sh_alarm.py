27ae29a09ec922af59496f0b5186a69dddfe9479
Christoph Gollok <christoph.gollok@alumni.fh-aachen.de>
2022-05-18 01:01:36 +0200

    add alarm sensor

---
+++ SHDevices/sh_alarm.py
@@ -0,0 +1,101 @@
+from SHDevices.sh_device import *
+
+class SH_Alarm(SHDevice):
+
+    def __init__(self,name, connection=None, device=None, states={}, fields={}):
+        super().__init__(name, connection, device, states, fields)        
+
+        # add Devices
+        self.sensor = device.getDevice("distance sensor")
+        self.sensor.enable(64)
+        self.triggered = False
+
+        # add states
+        super().add_state('armed',False)
+        super().add_state('armed_distance',0.0)
+        super().add_state('triggered',False)
+        super().add_state('on',False)
+        super().add_state('brightness',4.0)
+        
+
+        self.alarm_lamp = device.getSelf().getField("children").getMFNode(0)
+        # add fields
+        super().add_field('on', self.alarm_lamp.getField("on"))
+        super().add_field('brightness', self.alarm_lamp.getField("intensity"))
+        super().add_field('location', self.alarm_lamp.getField("location"))
+
+        self.fields['brightness'].setSFFloat(4.0)
+        self.fields['on'].setSFBool(False)
+        
+
+    
+    def setTriggered(self, triggered):
+        if self.getStateValue('triggered') != triggered and self.getStateValue('armed'):
+            self.log("triggered -> " + str(triggered))
+            self.setStateValue('triggered', triggered)
+            self.setOn(True)
+
+    def getTriggered(self):
+        return self.getStateValue('triggered')
+        
+    def getDistance(self):
+        return self.sensor.getValue()
+
+    def getArmedDistance(self):
+        return self.getStateValue('armed_distance') 
+    
+    def setArmedDistance(self, distance):
+        self.setStateValue('armed_distance', distance)
+
+    def getTriggerDistance():
+        return 
+
+    def getArmed(self):
+        return self.getStateValue('armed')
+    
+    def setArmed(self, armed):
+        self.setStateValue('armed', armed)
+        if armed:
+            self.setArmedDistance(self.getDistance())
+        else: 
+            self.setArmedDistance(0)
+            self.setOn(False)
+            self.setStateValue('triggered', False)
+
+    def setOn(self,on):
+        self.fields['on'].setSFBool(on)
+        self.setStateValue('on', on)
+
+    def getOn(self):
+        return self.getStateValue('on')
+
+    def setBrightness(self, value):
+        self.fields['brightness'].setSFFloat(value)
+        if value == 0:
+            self.setStateValue('on', False)
+            self.setStateValue('brightness', 0)
+        pass
+
+    def setState(self, name, value):    
+
+        match name:
+            case "trigger":
+                self.setTriggered(value)
+                pass
+            case "armed":
+                self.setArmed(value)
+                pass
+            case "on":
+                self.setOn(value)
+                pass
+            case _:
+                print("state not found or state is read only")
+                pass
+
+    def register(self):
+        super().register()
+
+    def reset(self):
+        super().reset()
+        self.sendReset()
+        pass
---
+++ controllers/alarm_controller/alarm_controller.py
@@ -0,0 +1,74 @@
+"""SH Controller Template controller."""
+
+from controller import Robot
+from controller import Supervisor
+
+from SHDevices.sh_alarm import *
+from SHDevices.iobroker_websocket import *
+
+from config.definitions import ROOT_DIR, _CFG
+
+def connected(ws):
+    global sh_device
+    sh_device.log("Connected")
+    sh_device.register()
+
+    
+def error(ws, error):
+    global sh_device
+    sh_device.log("Connection Error -> " + str(error))
+
+def closed(ws, close_status_code, close_msg):
+    global sh_device
+    sh_device.log("Disconected")
+    pass
+
+def web_message_cb(message):
+    global sh_device
+    message_cb(None, message)
+
+def message_cb(ws, message):
+    global sh_device
+    sh_device.log("new message -> " + str(message))
+    sh_device.setState(message["property"], message["value"])
+
+TIME_STEP = 64
+robot = Supervisor()
+timestep = int(robot.getBasicTimeStep())
+deltaTime = 0
+
+
+
+# create connection object
+ws = WebSocketClient(uri=_CFG["websocket"]["url"],open_cb=connected,
+                                                 close_cb=closed,
+                                                 message_cb=message_cb,
+                                                 error_cb=error)
+
+# create instance of SmartHome Device
+sh_device = SH_Alarm(robot.getName(), connection=ws, device=robot)
+sh_device.connect()
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    deltaTime += timestep
+    # check if messages are send from WebUI 
+    sh_device.receive_webui(web_message_cb)
+
+    # do something every 500ms
+    if deltaTime > 1000:
+        deltaTime = 0
+    
+        if sh_device.getArmed() and sh_device.getTriggered():
+            sh_device.setOn( not sh_device.getOn())
+
+        if sh_device.getDistance() != sh_device.getArmedDistance():
+            sh_device.setTriggered(True)
+        else: 
+            sh_device.setTriggered(False)
+        
+    pass
+
+
+# cleanup on Exit
